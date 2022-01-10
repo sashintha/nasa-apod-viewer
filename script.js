e12320842cbf42991e336b9a6d2e0b2842445adc
Sashintha Tennakoon <stennak@uwo.ca>
2022-01-10 01:51:14 -0500

    design revamp

--- script.js
+++ script.js
@@ -1,60 +1,53 @@
 
 async function ApiRequest(){
     let API_KEY = "FXyXePEFG09BA88Z0B0rpC9XnMAaezAs00qeacRe";
-    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=5`);
-
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=6`);
     let fulldata = response.url;
 
     fetch(fulldata)
     .then(res => res.json())
     .then((out) => {
-     console.log(out);
     useApiData(out);
-      
     })
     .catch(err => { throw err });
-
 }
 
 function useApiData(data){
+  for(let i = 0; i < 5; i+=3){
+  document.querySelector("#main").innerHTML +=
+    `
+      <div class="row row-cols-1 row-cols-md-3 g-4">
 
-  document.querySelector("#content1").innerHTML =
-  `
-  ${data[0].title}
-  <img class="card-img-top" src="${data[0].hdurl}">
-  <div class="card-body">
-    <p>Picture Of The Day: <b>${data[0].date}</b></p>
-    <p class="card-text"> ${data[0].explanation}</p>
-  </div>
-  `;
+          <div class="col inCard">
+            <div class="card h-100">
+              <img src="${data[i].url}" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">${data[i].title}</h5>
+                <p class="card-text">${data[i].explanation}</p>
+              </div>
+            </div>
+          </div>
 
-  document.querySelector("#content2").innerHTML =
-  `
-  ${data[1].title}
-  <img class="card-img-top" src="${data[1].hdurl}">
-  <div class="card-body">
-    <p>Picture Of The Day: <b>${data[1].date}</b></p>
-    <p class="card-text"> ${data[1].explanation}</p>
-  </div>
-  `;
+          <div class="col inCard">
+            <div class="card h-100">
+              <img src="${data[i+1].url}" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">${data[i+1].title}</h5>
+                <p class="card-text">${data[i+1].explanation}</p>
+              </div>
+            </div>
+          </div>
 
-  // document.querySelector("#content3").innerHTML =
-  // `
-  // ${data[2].title}
-  // <img class="card-img-top" src="${data[2].hdurl}">
-  // <div class="card-body">
-  //   <p>Picture Of The Day: <b>${data[2].date}</b></p>
-  //   <p class="card-text"> ${data[2].explanation}</p>
-  // </div>
-  // `;
-  // document.querySelector("#content4").innerHTML =
-  // `
-  // ${data[3].title}
-  // <img class="card-img-top" src="${data[3].hdurl}">
-  // <div class="card-body">
-  //   <p>Picture Of The Day: <b>${data[3].date}</b></p>
-  //   <p class="card-text"> ${data[3].explanation}</p>
-  // </div>
-  // `;
-
+          <div class="col inCard">
+            <div class="card h-100">
+              <img src="${data[i+2].url}" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">${data[i+2].title}</h5>
+                <p class="card-text">${data[i+2].explanation}</p>
+              </div>
+            </div>
+          </div>
+      </div>
+    `;
+  }
 }
