1d368e1d5a980f75345d8df80f30b020dfad7042
Sashintha Tennakoon <stennak@uwo.ca>
2022-01-10 15:16:40 -0500

    added loading animation

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
+const btn = document.querySelector("#submitInput");
+btn.addEventListener("click", ApiRequest);
+const loader = document.querySelector("#loading");
+let doneLoading = false;
+
+function displayLoading() {
+  loader.classList.add("display");
+  setTimeout(() => {
+      loader.classList.remove("display");
+  }, 5000);
+}
+
+function hideLoading(button) {
+  loader.classList.remove("display");
+}
 
 async function ApiRequest(){
+    if(doneLoading == false){
+      displayLoading();
+    }
+
     let API_KEY = "FXyXePEFG09BA88Z0B0rpC9XnMAaezAs00qeacRe";
-    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=6`);
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=18`);
     let fulldata = response.url;
 
     fetch(fulldata)
     .then(res => res.json())
     .then((out) => {
-    useApiData(out);
+      hideLoading()
+      doneLoading = true;
+      useApiData(out);
+      window.scrollTo(0,900);
     })
     .catch(err => { throw err });
 }
 
 function useApiData(data){
-  for(let i = 0; i < 5; i+=3){
+  for(let i = 0; i < 17; i++){
   document.querySelector("#main").innerHTML +=
     `
-      <div class="row row-cols-1 row-cols-md-3 g-4">
-
-          <div class="col inCard">
-            <div class="card h-100">
-              <img src="${data[i].url}" class="card-img-top">
-              <div class="card-body">
-                <h5 class="card-title">${data[i].title}</h5>
-                <p class="card-text">${data[i].explanation}</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col inCard">
-            <div class="card h-100">
-              <img src="${data[i+1].url}" class="card-img-top">
-              <div class="card-body">
-                <h5 class="card-title">${data[i+1].title}</h5>
-                <p class="card-text">${data[i+1].explanation}</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col inCard">
-            <div class="card h-100">
-              <img src="${data[i+2].url}" class="card-img-top">
-              <div class="card-body">
-                <h5 class="card-title">${data[i+2].title}</h5>
-                <p class="card-text">${data[i+2].explanation}</p>
-              </div>
-            </div>
-          </div>
+    <div class="fullCard">
+      <div class="card" style="width: 60rem;">
+        <img class="card-img-top" src="${data[i].url}" alt="${data[i].title}">
+        <div class="card-body">
+            <h5 class="card-title">${data[i].title}</h5>
+            <p class="card-text">${data[i].explanation}</p>
+            <button type="button" class="btn btn-danger btn-lg">Like</button>
+        </div>
       </div>
+    </div>
     `;
   }
 }
